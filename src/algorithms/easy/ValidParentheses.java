4063797e413e954e7a0d0dbdcce19d076390dc7c
jiapengcai <635793@gmail.com>
2018-01-20 20:43:50 +0800

    20.Valid Parentheses

---
+++ src/algorithms/easy/ValidParentheses.java
@@ -0,0 +1,31 @@
+package algorithms.easy;
+
+import java.util.Stack;
+
+/**
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ *
+ * @author caijiapeng
+ * @version created on 2018/1/20
+ */
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
