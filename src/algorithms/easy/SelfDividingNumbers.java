aa70c39972ea612a1cd2fe08ead8b971fdb05a6e
jiapengcai <635793@gmail.com>
2018-01-28 16:02:10 +0800

    728. Self Dividing Numbers

--- src/algorithms/easy/SelfDividingNumbers.java
+++ src/algorithms/easy/SelfDividingNumbers.java
@@ -23,14 +23,14 @@ public class SelfDividingNumbers {
 
     public List<Integer> selfDividingNumbers(int left, int right) {
         List<Integer> res = new ArrayList<>();
-        for (int i = left; i <= right; i++) {
-            for (int n = i; n > 0; n /= 10) {
-                if (n % 10 == 0 || i % (n % 10) != 0) {
+        for (int i = left, n = 0; i <= right; i++) {
+            for (n = i; n > 0; n /= 10) {
+                if ((n % 10 == 0) || (i % (n % 10) != 0)) {
                     break;
                 }
-                if (n == 0) {
-                    res.add(i);
-                }
+            }
+            if (n == 0) {
+                res.add(i);
             }
         }
         return res;
